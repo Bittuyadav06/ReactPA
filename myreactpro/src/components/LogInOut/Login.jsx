95ab542e69672d18b35416047fd0ca39dfebf1f1
Bittu yadav <bittuyadav01by@gmail.com>
2025-07-24 17:20:05 +0530

    Login page work

--- myreactpro/src/components/LogInOut/Login.jsx
+++ myreactpro/src/components/LogInOut/Login.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import {ImAccessiblity} from 'react-icons'
 import { Link, useNavigate } from 'react-router-dom';
 import Index from '../Index.jsx';
 import { useAuth } from '../../context/authContext.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export default function Login() {
-   
-    const {Login} = useAuth();
+
+    const { Login } = useAuth();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
@@ -32,45 +32,46 @@ export default function Login() {
         //     setMessage("Login failed! : Server Error");
 
         // }
-        
-  if (username === "admin" && password === "123") {
-    setMessage("Login successful! (fake token: abc123)");
-  } else {
-    setMessage("Login failed: Invalid credentials");
-  }
-navigate("/home")
+
+        if (username === "admin" && password === "123") {
+            setMessage("Login successful! (fake token: abc123)");
+        } else {
+            setMessage("Login failed: Invalid credentials");
+        }
+        navigate("/home")
 
     };
-   
+
 
     return (
         <>
             <div>
-                <div className="w-screen justify-items-center  py-4  bg-gray-300 h-screen content-center">
-                    <div className='from  md:w-[40%] md:h-[50%] mt-4 border rounded-md bg-blue-200 place-content-center justify-items-center  '
+                <div className="w-screen justify-items-center  py-4  bg-black bg-opacity-30 h-screen content-center">
+                    <div className='from  md:w-[40%] lg:w-[40%]  md:h-[50%] mt-4 border rounded-md bg-blue-200 place-content-center justify-items-center  '
                         onSubmit={handleLogin}>
-                        <p className='font-bold text-blue-700'> Login</p>
+                        <p className='font-bold text-blue-700'>  Login</p>
                         <div className='text-left'>
                             <label htmlFor="username" className='w-full text-left'>username</label>
-                            <input className='w-full' 
-                            type='text' 
-                            id="username" 
-                            value={username} 
-                            onChange={(e)=>setUsername(e.target.value)} required
+                            <input className='w-full'
+                                type='text'
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)} required
                             />
                         </div>
                         <div className='text-left'>
                             <label htmlFor="password">password</label>
-                            <input className='w-full' type='text' id="password" 
-                            value={password} 
-                            onChange={(e)=>setPassword(e.target.value)} required
+                            <input className='w-full' type='text' id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} required
                             />
                         </div>
-                        <div className=' py-2  w-3/5 flex justify-between  '>
-                            <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white  ' type='submit' onClick={handleLogin} ><Link to="/Index" >Login </Link></button>
+                        <div className=' py-2 md:w-[80%] xl:w-[80%] flex justify-between   '>
+                            <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white  ' type='submit'
+                                onClick={handleLogin} ><Link to="/Index" >Login </Link></button>
                             <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white  ' >logout</button>
                         </div>
-                        {message && <p style={{marginTop:"10px"}}>{message}</p>}
+                        {message && <p style={{ marginTop: "10px" }}>{message}</p>}
                     </div>
                 </div>
             </div>
