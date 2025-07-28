a4161426060d38ac91abaa340a656bbb02eacf93
Bittu yadav <bittuyadav01by@gmail.com>
2025-07-28 17:05:42 +0530

    Form page add

--- myreactpro/src/components/LogInOut/Login.jsx
+++ myreactpro/src/components/LogInOut/Login.jsx
@@ -45,11 +45,20 @@ export default function Login() {
 
     return (
         <>
-            <div>
+            
                 <div className="w-screen justify-items-center  py-4  bg-black bg-opacity-30 h-screen content-center">
-                    <div className='from  md:w-[40%] lg:w-[40%]  md:h-[50%] mt-4 border rounded-md bg-blue-200 place-content-center justify-items-center  '
+                    <div className='from md:w-[40%] lg:w-[40%] md:h-[50%] mt-4 border rounded-md bg-blue-200 place-content-center justify-items-center  '
                         onSubmit={handleLogin}>
-                        <p className='font-bold text-blue-700'> <FaUser /> Login</p>
+                        <div className='md:w-[100px] align-center'>
+                            {/* <span className='font-bold text-blue-700'><FaUser/></span>
+                        <span className='font-bold text-blue-700'> Login</span> */}
+                        <FaUser className='text-blue-700 '/> 
+                        </div>
+                          <div className='md:w-[100px] align-center'>
+                                
+                        <span className='font-bold text-blue-700'> Login</span>
+                         
+                        </div>
                         <div className='text-left'>
                             <label htmlFor="username" className='w-full text-left'>username</label>
                             <input className='w-full'
@@ -66,15 +75,15 @@ export default function Login() {
                                 onChange={(e) => setPassword(e.target.value)} required
                             />
                         </div>
-                        <div className=' py-2 md:w-[80%] xl:w-[80%] flex justify-between   '>
-                            <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white  ' type='submit'
+                        <div className=' py-2  '>
+                            <button className='bg-blue-300 rounded-md pl-2 pr-2 hover:text-white' type='submit'
                                 onClick={handleLogin} ><Link to="/Index" >Login </Link></button>
-                            <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white  ' >logout</button>
+                            {/* <button className='bg-blue-300 rounded-md pl-2 pr-2  hover:text-white'>logout</button> */}
                         </div>
                         {message && <p style={{ marginTop: "10px" }}>{message}</p>}
                     </div>
                 </div>
-            </div>
+            
         </>
     )
 }
